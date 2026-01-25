464eee67cca85899a3600ed810df141fa5558b65
milandashscripl <milkmilan83@gmail.com>
2026-01-25 11:06:24 +0530

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
-const cors = require("cors");
+import cors  from "cors";
 
 app.use(cors({
   origin: "*",   // allow all (for now)

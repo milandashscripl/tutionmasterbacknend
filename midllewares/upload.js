7d409d277a29ad79cd4c8fcadfbcf14a0adc941d
milandashscripl <immilankumar01@gmail.com>
2026-02-14 17:55:02 +0530

    upload image

--- midllewares/upload.js
+++ midllewares/upload.js
@@ -2,14 +2,9 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image/")) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image files allowed"), false);
-  }
-};
-
-const upload = multer({ storage, fileFilter });
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
 
 export default upload;
